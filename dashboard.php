a964db273e3a7de3024888f146f1a7c0e3d2c4da
ROZI RIKZA AKBAR(572633) <05211850010003@mahasiswa.integra.its.ac.id>
2020-04-23 22:12:04 +0700

    pagination

--- dashboard.php
+++ dashboard.php
@@ -27,9 +27,8 @@ GROUP BY tbl1.id_pendaftaran");
         <h3 class="card-title">Data Persebaran Penyakit</h3>
 
         <div class="card-tools">
-          <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
-          </button>
-          <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
+          <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
+          <button type="button" class="btn btn-tool" data-card-widget="remove" disabled><i class="fas fa-times"></i></button>
         </div>
       </div>
       <div class="card-body">
